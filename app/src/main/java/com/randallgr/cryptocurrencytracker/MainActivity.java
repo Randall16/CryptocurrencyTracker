c9068d9c3cd780f17d766c1f90a730ee446dddd9
Randall <randallgr5@outlook.com>
2018-08-12 03:48:30 -0400

    Created and initialized action bar spinner

--- app/src/main/java/com/randallgr/cryptocurrencytracker/MainActivity.java
+++ app/src/main/java/com/randallgr/cryptocurrencytracker/MainActivity.java
@@ -4,14 +4,25 @@ package com.randallgr.cryptocurrencytracker;
 import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner cryptoSelectionSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cryptoSelectionSpinner = findViewById(R.id.s_cryptoSelection);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(),
+                R.array.supportedCryptocurrencies, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cryptoSelectionSpinner.setAdapter(adapter);
 
 
     }
