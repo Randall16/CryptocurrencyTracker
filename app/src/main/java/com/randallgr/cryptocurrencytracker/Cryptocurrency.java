f42f26a10aabd37af37eabd1b50bc9e82abc0439
Randall <randallgr5@outlook.com>
2018-09-04 09:44:44 -0400

    Added a new fetch method to increase accuracy of 7 day and 30 day change

--- app/src/main/java/com/randallgr/cryptocurrencytracker/Cryptocurrency.java
+++ app/src/main/java/com/randallgr/cryptocurrencytracker/Cryptocurrency.java
@@ -19,11 +19,13 @@ public abstract class Cryptocurrency {
 
     protected String domesticCurrency;
     protected double currentPrice, hourChange, dayChange, weekChange, monthChange, yearChange;
-    protected double [] dailyPrices, intraDayPrices, intraHourPrices;
+    protected double [] intraYearPrices, intraDayPrices, intraHourPrices;
+    protected double [] intraWeekPrices, intraMonthPrices;
     protected OnFetchesCompleteListener mListener;
 
     private static RequestQueue volleyQueue = null;
-    private JsonObjectRequest jsonReqCurrentPrice, jsonReqDailyPrices, jsonReqIntraDayPrices;
+    private JsonObjectRequest jsonReqCurrentPrice, jsonReqDailyPrices;
+    private JsonObjectRequest jsonReqIntraDayPrices, jsonReqIntraMonthPrices;
     private int fetchCounter;
     private static final String EXCHANGE = "CCCAGG";
 
@@ -33,9 +35,12 @@ public abstract class Cryptocurrency {
         jsonReqDailyPrices = null;
         jsonReqIntraDayPrices = null;
         jsonReqCurrentPrice = null;
-        dailyPrices = new double[365];   // initializing arrays
+        jsonReqIntraMonthPrices = null;
+        intraYearPrices = new double[365];   // initializing arrays
         intraDayPrices = new double[288];
         intraHourPrices = new double[60];
+        intraWeekPrices = new double[168];
+        intraMonthPrices = new double[240];
 
         if(volleyQueue == null)
             volleyQueue = Volley.newRequestQueue(c);  // initializing the volleyQueue
@@ -90,11 +95,11 @@ public abstract class Cryptocurrency {
 
 
     /**
-     * The fetchDailyPrices method will return a JSON object containing the cryptocurrency's
+     * The fetchIntraYearPrices method will return a JSON object containing the cryptocurrency's
      * closing price in daily time intervals over the past year. This JSON object will then be
-     * parsed to populate the dailyPrices array.
+     * parsed to populate the intraYearPrices array.
      */
-    protected void fetchDailyPrices() {
+    protected void fetchIntraYearPrices() {
         // Using CryptoCompare's histoday
         final String url = "https://min-api.cryptocompare.com/data/histoday?fsym=" +
                 getTicker() + "&tsym=" + domesticCurrency + "&limit=365&aggregate=1&e=" + EXCHANGE;
@@ -104,7 +109,7 @@ public abstract class Cryptocurrency {
                 url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                parseDailyPrices(response);
+                parseIntraYearPrices(response);
                 updateFetchCounter();
             }
         }, new Response.ErrorListener() {
@@ -117,7 +122,7 @@ public abstract class Cryptocurrency {
         volleyQueue.add(jsonReqDailyPrices);
     }
 
-    private void parseDailyPrices(JSONObject response) {
+    private void parseIntraYearPrices(JSONObject response) {
 
         JSONArray data = null;
 
@@ -131,7 +136,7 @@ public abstract class Cryptocurrency {
         for(int i = 0; i < 365; i++) {
 
             try {
-                dailyPrices[i] = data.getJSONObject(364 - i).getDouble("close");
+                intraYearPrices[i] = data.getJSONObject(364 - i).getDouble("close");
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -223,12 +228,84 @@ public abstract class Cryptocurrency {
 
     }
 
+    private void fetchIntraMonthPrices() {
+
+        // using CryptoCompare's histohour
+        final String url = "https://min-api.cryptocompare.com/data/histohour?fsym=" + getTicker() +
+                "&tsym=" + domesticCurrency + "&limit=720&aggregate=1&e=" + EXCHANGE;
+
+        jsonReqIntraMonthPrices = new JsonObjectRequest(Request.Method.GET,
+                url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                parseIntraMonthPrices(response);
+                parseIntraWeekPrices(response);
+                updateFetchCounter();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("tester", error.getMessage());
+            }
+        });
+
+        volleyQueue.add(jsonReqIntraMonthPrices);
+    }
+
+    private void parseIntraWeekPrices(JSONObject response) {
+
+        JSONArray data = null;
+
+        try {
+            data = response.getJSONArray("Data");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        for(int i = 0; i < 168; i++) {
+            try {
+                intraWeekPrices[i] = Double.valueOf(data.getJSONObject(719-i)
+                        .getString("close"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void parseIntraMonthPrices(JSONObject response) {
+
+        JSONArray data = null;
+        int arrayCounter = 240, k = 3;
+
+        try {
+            data = response.getJSONArray("Data");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // array data has 720 elements but we are only storing every 3rd element
+        for(int i = 0; i < 720; i++) {
+            if(k++ == 3) {
+                try {
+                    intraMonthPrices[--arrayCounter] = data.getJSONObject(i)
+                            .getDouble("close");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                k = 1;  // reset k
+            }
+        }
+
+    }
+
     public void fetchAll() {
 
         cancelJsonReqs();
         volleyQueue.getCache().clear();
         fetchCounter = 0;
-        fetchDailyPrices();
+        fetchIntraYearPrices();
+        fetchIntraMonthPrices();
         fetchIntraDayPrices();
         fetchCurrentPrice();
     }
@@ -239,7 +316,8 @@ public abstract class Cryptocurrency {
         volleyQueue.getCache().invalidate(jsonReqCurrentPrice.getCacheKey(), true);
         volleyQueue.getCache().invalidate(jsonReqIntraDayPrices.getCacheKey(), true);
 
-        fetchCounter = 1;
+        // Only refreshing the current price and intraHour prices so starting fetchCounter at 2
+        fetchCounter = 2;
 
         fetchIntraDayPrices();
         fetchCurrentPrice();
@@ -248,18 +326,19 @@ public abstract class Cryptocurrency {
     private void cancelJsonReqs() {
 
         if(jsonReqCurrentPrice != null && jsonReqIntraDayPrices != null
-                && jsonReqDailyPrices != null) {
+                && jsonReqDailyPrices != null && jsonReqIntraMonthPrices != null) {
 
             jsonReqCurrentPrice.cancel();
             jsonReqIntraDayPrices.cancel();
             jsonReqDailyPrices.cancel();
+            jsonReqIntraMonthPrices.cancel();
         }
     }
 
     private void updateFetchCounter() {
         fetchCounter++;
 
-        if(fetchCounter >= 3) {
+        if(fetchCounter >= 4) {
             calculatePercentChanges();
             mListener.onFetchesComplete();
         }
@@ -269,9 +348,9 @@ public abstract class Cryptocurrency {
         // Using percent change formula
         hourChange = ( (currentPrice - intraHourPrices[59]) / intraHourPrices[59] ) * 100;
         dayChange = ( ( currentPrice - intraDayPrices[287]) / intraDayPrices[287] ) * 100;
-        weekChange = ( (currentPrice - dailyPrices[6]) / dailyPrices[6] ) * 100;
-        monthChange = ( (currentPrice - dailyPrices[29]) / dailyPrices[29] ) * 100;
-        yearChange = ( (currentPrice - dailyPrices[364]) / dailyPrices[364] ) * 100;
+        weekChange = ( (currentPrice - intraWeekPrices[167]) / intraWeekPrices[167] ) * 100;
+        monthChange = ( (currentPrice - intraMonthPrices[239]) / intraMonthPrices[239] ) * 100;
+        yearChange = ( (currentPrice - intraYearPrices[364]) / intraYearPrices[364] ) * 100;
     }
 
     // GET METHODS
@@ -309,7 +388,21 @@ public abstract class Cryptocurrency {
 
     public double getIntraYearPrice(int daysAgo) {
         if(daysAgo >= 1 && daysAgo <= 365)
-            return dailyPrices[daysAgo-1];
+            return intraYearPrices[daysAgo-1];
+        else
+            return -1;
+    }
+
+    public double getIntraMonthPrice(int threeHoursAgo) {
+        if(threeHoursAgo >= 1 && threeHoursAgo <= 240)
+            return  intraMonthPrices[threeHoursAgo-1];
+        else
+            return -1;
+    }
+
+    public double getIntraWeekPrice(int hoursAgo) {
+        if(hoursAgo >= 1 && hoursAgo <= 168)
+            return intraWeekPrices[hoursAgo-1];
         else
             return -1;
     }
